dc0d5a152ce827fb67ef579b188beecd14b7c3f5
Julie Judachina <iuliia.saunova@email.cz>
2021-06-28 00:20:09 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './sass/main.scss';
+import menuTemplates from './teamplates/menu.hbs';
+import menu from './menu.json';
+
+
+
+const menuBoard = document.querySelector('.js-menu');
+menuBoard.innerHTML = menuTemplates(menu);
